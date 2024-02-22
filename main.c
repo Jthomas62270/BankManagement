a055bc4737593bef33f26a084e023e3e9c6b6b59
Jthomas62270 <Jthomas62270@gmail.com>
2024-02-21 22:54:36 -0500

    First Commit; Added Function Headers

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+Author: John Garcia 
+Last Modified: Feb. 21, 2024
+Project Desc: Banking System using C
+*/
+#include <stdio.h>
+#include <stdlib.h> 
+
+//Account operations 
+int withdraw(int); 
+int deposit(int); 
+void checkBalance(int); 
+
+//User operations
+void addUser(int); 
+void removeUser(int); 
+
+//Application operations
+void menu(void); 
+
+
+
+
+
+// Main
+int main() { 
+    return 0; 
+}
